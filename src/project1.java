cf78583aba2964f5cb7ee65eddb7f701e4d3ada8
Nithinbs18 <nithinbs18@gmail.com>
2019-08-17 09:36:20 +0200

    nithin commit

--- src/project1.java
+++ src/project1.java
@@ -1,6 +1,6 @@
 public class project1 {
     public static void main(String[] args) {
-        System.out.println("hello navanith");
+        System.out.println("hello navanith from Nithin");
 
     }
 }
