2ffae2011c53df74a6a73b32442343caf6dc8adb
Navanithbs <navanithbhardwaj@gmail.com>
2019-08-18 11:48:31 -0700

    refactor

---
+++ project.3/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter n:");
+        n=scan.nextInt();
+        for(int i=1;i<=10;i++) {
+            System.out.println(+n + " x " + i + " = " + (n * i));
+        }
+
+    }
+}
